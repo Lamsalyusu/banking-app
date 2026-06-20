58db5cb719eae9d7a1ed0c5405cadb9692c43361
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-06-20 14:22:53 +0545

    used , instead of .

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ const authMiddleware = (req,res,next) =>
     }
     const token = authHeader.split(' ')[1];
     try{
-        const decode = jwt.verify(token.process.env.JWT_SECRET);;
+        const decode = jwt.verify(token,process.env.JWT_SECRET);;
         req.user = decode;
         next();
     } catch (error)
