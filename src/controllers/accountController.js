2a0d7fef8165c3045662fcfd8867a38ea7e8f7c7
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-06-20 21:41:48 +0545

    account controller

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -79,9 +79,52 @@ const accountController = {
     transfer:async(req,res)=>{
         try{
             const userId = req.user.id;
-            const { amount } = req.body;
-            
+            const { amount,receiverAccountNumber } = req.body;
+            if(!receiverAccountNmber){
+                return sessionStorage.status(400).json({error:'Recipient Account number is required'})
+            }
+            if (!amount || isNaN(amount) || amount <= 0) 
+                {
+                return res.status(400).json({ error: 'Please provide a valid transfer amount greater than 0' });
+            }
+            const senderAccount = await AccountModel.findByUserID(userId);
+            if(!senderAccount){
+                return res.status(404).json({error:'sender account not found'});
+            }
+            const receiverAccount = await AccountModel.findByAccountNumber(receiverAccountNumber);
+            if (!receiverAccount){
+                return res.status(404).json({error:'Receiver not found'});
+            }
+            if (senderAccount.id === receiverAccount.id) {
+            return res.status(400).json({ error: 'Cannot transfer to your own account' });
+            }
+            if (senderAccount.balance < amount) {
+            return res.status(400).json({ error: 'Insufficient funds' });
+            }
+
+            await db.query('BEGIN');
+            const deductQuery = 'UPDATE accounts SET balance = balance - $1 WHERE id = $2 RETURNING *';
+            const senderUpdated = await db.query(deductQuery, [amount, senderAccount.id]);
+            const addQuery = 'UPDATE accounts SET balance = balance + $1 WHERE id = $2 RETURNING *';
+            await db.query(addQuery,[amount,receiverAccount.id]);
+
+            await TransactionModel.createTransaction(
+                senderAccount.id,
+                receiverAccount.id,
+                amount,
+                'transfer',
+                'Transfer'
+            );
+
+            await db.query('COMMIT');
+            res.status(200).json({
+                message:'Transfer Successful',
+                newBalance:senderUpdated.rows[0].balance
+            });
+
+        }catch(error){
+            await db.query('ROLLBACK');
+            res.status(500).json({error:error.message});
         }
     }
-
 };
